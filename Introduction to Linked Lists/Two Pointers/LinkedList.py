9dedf210a37e63afd7845b77a08dff0c1555b787
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2022-11-28 01:18:50 +0300

    Create LinkedList.py

---
+++ Introduction to Linked Lists/Two Pointers/LinkedList.py
@@ -0,0 +1,43 @@
+from Node import Node
+
+
+class LinkedList:
+    def __init__(self):
+        self.head_node = None
+
+    def get_head(self):
+        return self.head_node
+
+    def is_empty(self):
+        if(self.head_node is None):  # Check whether the head is None
+            return True
+        else:
+            return False
+
+    def insert_at_head(self, dt):
+        temp_node = Node(dt)
+        temp_node.next_element = self.head_node
+        self.head_node = temp_node
+        return self.head_node
+
+    def print_list(self):
+        if(self.is_empty()):
+            print("List is Empty")
+            return False
+        temp = self.head_node
+        while temp.next_element is not None:
+            print(temp.data, end=" -> ")
+            temp = temp.next_element
+        print(temp.data, "-> None")
+        return True
+
+    def length(self):
+        # start from the first element
+        curr = self.get_head()
+        length = 0
+
+        # Traverse the list and count the number of nodes
+        while curr is not None:
+            length += 1
+            curr = curr.next_element
+        return length
