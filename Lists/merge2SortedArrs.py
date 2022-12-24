b5157864ddc9b5ba66429e4a2239ea24f2bfe2f7
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2022-12-24 23:43:15 +0300

    Update merge2SortedArrs.py

--- Lists/merge2SortedArrs.py
+++ Lists/merge2SortedArrs.py
@@ -12,3 +12,24 @@ def merge_lists(lst1, lst2):
     if(ind2 < len(lst2)):
         lst1.extend(lst2[ind2:])
     return lst1
+
+
+
+#solve it using bubble sort
+
+def sort_and_merge(array1):
+  # Use bubble sort to sort array1
+  n = len(array1)
+  for i in range(n):
+    for j in range(0, len(array1)-1):
+      if array1[j] > array1[j + 1]:
+        temp = array1[j]
+        array1[j] = array1[j + 1]
+        array1[j + 1] = temp
+        
+    return array1
+    
+    
+    
+list1=[1,4,6,7,2]
+print(sort_and_merge(list1))
